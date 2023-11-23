16ca3ed82d6ae072652907c9eea774f9151631f5
Riddhi0312 <151597147+Riddhi0312@users.noreply.github.com>
2023-11-23 11:39:23 +0530

    Add files via upload

---
+++ result.js
@@ -0,0 +1,20 @@
+﻿﻿window.addEventListener('load', () => {
+
+    // Via Query parameters - GET
+    /* const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const surname = params.get('surname'); 
+    const email = params.get('email'); */
+
+    // Via local Storage
+    /* const name = localStorage.getItem('NAME');
+    const surname = localStorage.getItem('SURNAME');
+    const email = localStorage.getItem('EMAIL'); */
+    
+    const name = sessionStorage.getItem('NAME');
+    const surname = sessionStorage.getItem('SURNAME');
+    
+    document.getElementById('result-name').innerHTML = name;
+    document.getElementById('result-surname').innerHTML = surname;
+
+})
